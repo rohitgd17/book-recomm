2067ef8ac8cf817ac7cad78e678a5839bfb152f0
rohitgd17 <rohitdhengale315@gmail.com>
2024-12-02 18:56:23 +0530

    Remove all tracked files

--- New folder/java/com/book_recommendation/system/repositiries/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.book_recommendation.system.repositiries;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book_recommendation.system.entities.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/com/book_recommendation/system/repositiries/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.book_recommendation.system.repositiries;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book_recommendation.system.entities.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
